698bff99eee3634a23970d4e4e13daaa5e12c75e
etay1 <etay1@brockport.edu>
2023-10-10 16:37:14 -0400

    [ADD] login page

--- fitness_app/src/components/Login/Login.jsx
+++ fitness_app/src/components/Login/Login.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import './Login.css';
+import photo from '../../images/photo.jpg';
 
 export const Login = () => {
     return (
-        <div className='main'>
-            <div className="title">
-                <h1>Fitness Pal</h1>
+        <div className='container'>
+            <div className='box left-box'>
+                    <div className="form">
+                        <input type="text" id="username" placeholder="Username" className="input-field" />
+                        <input type="password" id="password" placeholder="Password" className="input-field" />
+                        <button type="submit" id="login_btn" className="login-btn">Login</button>                
+                    </div>
             </div>
-            <div className="container_login">
-                <input type="text" id="username" placeholder="Username" className="input-field" />
-                <input type="password" id="password" placeholder="Password" className="input-field" />
-                <button type="submit" id="login_btn" className="login-btn">Login</button>
+            <div className='box right-box'>
+            right box
             </div>
         </div>
     );
