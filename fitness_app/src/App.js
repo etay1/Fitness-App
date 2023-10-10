52226d48fd05d6f515868947863ed365df108836
etay1 <etay1@brockport.edu>
2023-10-10 19:02:18 -0400

    [ADD] base layout: login prototype
    Created a supabase client
    Center layout for login page

--- fitness_app/src/App.js
+++ fitness_app/src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
 import { Login } from './components/Login/Login';
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(process.env.REACT_APP_SUPABASE_URL,process.env.REACT_APP_ANON_KEY);
+console.log(supabase)
+console.log(process.env.REACT_APP_SUPABASE_URL)
+console.log(process.env.REACT_APP_ANON_KEY) 
 
 function App() {
   return (
