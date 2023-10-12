68ba635da1de1521c9a8aa1685b96bed6a2aae3f
etay1 <etay1@brockport.edu>
2023-10-12 13:07:37 -0400

    [ADD] accessiblity to  user data

--- fitness_app/src/components/Dashboard/Dashboard.jsx
+++ fitness_app/src/components/Dashboard/Dashboard.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 import './Dashboard.css';
 
 export const Dashboard = ({supabase, session}) => {
+    console.log(session)
+    console.log(session.user.email)
+    console.log(session.user.id)
+    
+    const userId = session.user.id;
+    console.log(userId)
+
     return (
         <div className='dashboard'>
             <h1>Welcome {session.user.email}</h1>
+
             <button onClick={() => supabase.auth.signOut()}>Sign Out</button>
         </div>
     );
