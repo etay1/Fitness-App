4676a82317fffb8a6b2d2d201a108b01fbd8cd7f
etay1 <etay1@brockport.edu>
2023-10-18 18:09:17 -0400

    [ADD] add an exercise component

--- fitness_app/src/components/AddExercise/AddExercise.jsx
+++ fitness_app/src/components/AddExercise/AddExercise.jsx
@@ -1,28 +1,123 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './AddExercise.css';
 
-const AddExercise = () => {
+function AddExercise() {
+  // State initialization
+  const [category, setCategory] = useState('cardio');
+  const [exerciseData, setExerciseData] = useState({
+    exerciseName: '',
+    description: '',
+    caloriesPerRep: 0,
+    caloriesPerDuration: 0,
+  });
+  const [successMessage, setSuccessMessage] = useState('');
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  // Event handlers
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setExerciseData({ ...exerciseData, [name]: value });
+  };
+
+  const handleCategoryChange = (newCategory) => {
+    setCategory(newCategory);
+  };
+
+  const navigateToMainMenu = () => {
+    window.location.href = '/';
+    // You can use React Router as well here.
+  };
+
+  const handleAddExercise = () => {
+    // Code to send the exercise data to the server will go here.
+    setSuccessMessage(`Successfully added ${category === 'cardio' ? 'Cardio' : 'Strength'} Exercise.`);
+    setIsSuccess(true);
+    setExerciseData({
+      exerciseName: '',
+      description: '',
+      caloriesPerRep: 0,
+      caloriesPerDuration: 0,
+    });
+  };
+
   return (
-    <div className='page'>
-        <div className='container'>
+    <div className="page">
+      <div className="container">
+        <div className="exercise-form">
+          <div className="category-toggle">
+            <button
+              className={`category-button ${category === 'cardio' ? 'active' : 'inactive'}`}
+              onClick={() => handleCategoryChange('cardio')}
+            >
+              Cardio
+            </button>
+            <button
+              className={`category-button ${category === 'strength' ? 'active' : 'inactive'}`}
+              onClick={() => handleCategoryChange('strength')}
+            >
+              Strength
+            </button>
+          </div>
 
-          {/* THIS IS THE BLUEPRINT FOR ADDING AN EXERCISE
-              THE LEFT BOX AND RIGHT BOX WILL BE ADDING A 
-              CARDIO OR STRENGTH EXERCISE*/}
-
-            {/* left box */}
-            <div className='box left-box'>
-              <p>left box</p>
-              </div>
-
-            {/* right box */}
-            <div className='box right-box'>
-              <p>right box</p>
+          {/* Form */}
+          <form>
+            <div>
+              <label>Exercise Name:</label>
+              <input
+                type="text"
+                name="exerciseName"
+                value={exerciseData.exerciseName}
+                onChange={handleInputChange}
+              />
             </div>
 
-        </div>
+            {category === 'strength' && (
+              <div>
+                <label>Calories/15 minutes:</label>
+                <input
+                  type="number"
+                  name="caloriesPerRep"
+                  value={exerciseData.caloriesPerRep}
+                  onChange={handleInputChange}
+                />
+              </div>
+            )}
+            {category === 'cardio' && (
+              <div>
+                <label>Calories per Duration:</label>
+                <input
+                  type="number"
+                  name="caloriesPerDuration"
+                  value={exerciseData.caloriesPerDuration}
+                  onChange={handleInputChange}
+                />
+              </div>
+            )}
+            <div>
+              <label>Description:</label>
+              <textarea
+                name="description"
+                value={exerciseData.description}
+                onChange={handleInputChange}
+              />
+            </div>
 
+            <button className="button-add-exercise" type="button" onClick={handleAddExercise}>
+              Add {category === 'cardio' ? 'Cardio' : 'Strength'} Exercise
+            </button>
+
+            <button className="button-add-exercise" onClick={navigateToMainMenu}>
+              Main Menu
+            </button>
+
+            {isSuccess && (
+              <div className="success-message">{successMessage}</div>
+            )}
+          </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default AddExercise
+export default AddExercise;
