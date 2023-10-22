21bc8ceb127c347ddd0187178184e582243e10f5
Perry512 <Perry512@hotmail.com>
2023-10-22 01:59:34 -0400

    [ADD] AddUserWeight: added space to input weight along with css

--- fitness_app/src/components/AddUserWeight/AddUserWeight.jsx
+++ fitness_app/src/components/AddUserWeight/AddUserWeight.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from 'react';
+import './AddUserWeight.css';
 
 export const AddUserWeight = () => {
   return (
     <div className='page'>
         <div className='container'>
+          <div className='weight-add-form'>
+            <h1 className='weight-add-title'> Add a weight </h1>
+
+            <form>
+              <div className='input-container'>
+                <label>Current Weight:</label>
+                <input
+                  type="number"
+                  name="weight"
+                  //TODO: add value along the lines of value={foo.bar}
+                />
+              </div>
+            </form>
             <p>Add user weight boiler plate</p>
+          </div>
         </div>
     </div>
   )
