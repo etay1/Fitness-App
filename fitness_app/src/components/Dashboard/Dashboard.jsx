c39edff197f3736ee0eb69233a6ef1a2337a2c00
Faizan_Rafieuddin <frafi1@brockport.edu>
2023-10-23 19:47:52 -0400

    [ADD] In process of curating the weekly, yearly, and monthly data and display graphing based on that

--- fitness_app/src/components/Dashboard/Dashboard.jsx
+++ fitness_app/src/components/Dashboard/Dashboard.jsx
@@ -6,51 +6,46 @@ export const Dashboard = ({ supabase, session }) => {
   const [weights, setWeights] = useState([]);
   const [dates, setDates] = useState([]);
   const [chartData, setChartData] = useState({});
+  const [weightMap, setWeightMap] = useState({});
+  const [selectedTimeRange, setSelectedTimeRange] = useState("weekly"); // Default to "weekly"
 
   useEffect(() => {
     async function fetchWeightData() {
       // Fetch all weights and dates for the user
       const { data, error } = await supabase
-      .from("user_weight")
-      .select("weight, date") 
-      .eq("user_id", session.user.id);
+        .from("user_weight")
+        .select("weight, date")
+        .eq("user_id", session.user.id);
 
+      if (error) {
+        console.error(error);
+      } else {
+        const weightValues = data.map((item) => item.weight);
+        let dateValues = data.map((item) => item.date);
+        dateValues = dateValues.sort((a, b) => new Date(b) - new Date(a));
+        const weightMap = {};
+        dateValues.forEach((date, index) => {
+          weightMap[date] = weightValues[index];
+        });
 
-        if (error) {
-          console.error(error);
-        } else {
-          const weightValues = data.map((item) => item.weight);
-          setWeights(weightValues);
-          const dateValues = data.map((item) => item.date);
-          setDates(dateValues);
-
-        }
+        setWeights(weightValues);
+        setDates(dateValues);
+        setWeightMap(weightMap);
+      }
     }
     fetchWeightData();
   }, [supabase, session.user.id]);
 
-  const monthNames = {
-    1: "January",
-    2: "February",
-    3: "March",
-    4: "April",
-    5: "May",
-    6: "June",
-    7: "July",
-    8: "August",
-    9: "September",
-    10: "October",
-    11: "November",
-    12: "December",
-  };
+  const weightMapKeys = Object.keys(weightMap);
+  const weightMapValues = Object.values(weightMap);
 
   useEffect(() => {
     const data = {
-      labels: dates,
+      labels: weightMapKeys,
       datasets: [
         {
           label: "Weight",
-          data: weights,
+          data: weightMapValues,
           fill: false,
           borderColor: "rgb(75, 192, 192)",
           tension: 0.1,
@@ -61,15 +56,17 @@ export const Dashboard = ({ supabase, session }) => {
   }, [weights]);
 
   useEffect(() => {
-    const chart = new Chart("myChart", {
-      type: "line",
-      data: chartData,
-    });
-    return () => chart.destroy();
-  }, [chartData]);
+    if (selectedTimeRange === "yearly") {
+      // Render the chart only when "Yearly" is selected
+      const chart = new Chart("myChart", {
+        type: "line",
+        data: chartData,
+      });
+      return () => chart.destroy();
+    }
+  }, [selectedTimeRange, chartData]);
 
   return (
-    
     <div className="dashboard">
       <h1>Welcome {session.user.email}</h1>
       <button onClick={() => supabase.auth.signOut()}>Sign Out</button>
@@ -78,6 +75,38 @@ export const Dashboard = ({ supabase, session }) => {
       <Link to="/add-user-weight">Add User Weight</Link>
       <div className="weight_chart">
         <canvas id="myChart" width={100} height={50}></canvas>
+      </div>
+      <div className="weight_range_buttons">
+        <label className="radio-button">
+          <input
+            type="radio"
+            name="time-range"
+            value="weekly"
+            checked={selectedTimeRange === "weekly"}
+            onChange={() => setSelectedTimeRange("weekly")}
+          />
+          Weekly
+        </label>
+        <label className="radio-button">
+          <input
+            type="radio"
+            name="time-range"
+            value="monthly"
+            checked={selectedTimeRange === "monthly"}
+            onChange={() => setSelectedTimeRange("monthly")}
+          />
+          Monthly
+        </label>
+        <label className="radio-button">
+          <input
+            type="radio"
+            name="time-range"
+            value="yearly"
+            checked={selectedTimeRange === "yearly"}
+            onChange={() => setSelectedTimeRange("yearly")}
+          />
+          Yearly
+        </label>
       </div>
     </div>
   );
