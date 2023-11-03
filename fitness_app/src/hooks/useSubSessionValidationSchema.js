57130e978914fe28ce2d89661ca45227c955dfc6
etay1 <etay1@brockport.edu>
2023-11-02 22:07:55 -0400

    [FIX] syntax errors
    Next step fix logical errors

--- fitness_app/src/hooks/useSubSessionValidationSchema.js
+++ fitness_app/src/hooks/useSubSessionValidationSchema.js
@@ -3,33 +3,38 @@ import * as Yup from "yup";
 import { useSuccessMessage } from "./useSuccessMessage";
 
 export default function useSubSessionValidationSchema(
-    category, updateSuccessMessage) {
-    const [validationSchema, setValidationSchema] = useState(null);
-    const [key, setKey] = useState(0);
+  category,
+  updateSuccessMessage
+) {
+  const [validationSchema, setValidationSchema] = useState(null);
+  const [key, setKey] = useState(0);
 
-    useEffect(() => {    
-        if (category === "strength") {
-            setValidationSchema(
-                Yup.object().shape({
-                    exerciseName: Yup.string().required("Exercise Name is required"),
-                    startTime: Yup.startTime().required("Start Time is required"),
-                    endTime: Yup().endTime().required("End Time is required"),
-                    sets: Yup.number().required.min(1, "Sets must be greater than 0"),
-                    repsPerSet: Yup.number().required.min(1, "Reps must be greater than 0"),
-                })
-            );
-        } else if (category === "cardio") {
-            setValidationSchema(
-                Yup.object().shape({
-                    exerciseName: Yup.string().required("Exercise Name is required"),
-                    startTime: Yup.startTime().required("Start Time is required"),
-                    endTime: Yup().endTime().required("End Time is required"),
-                })
-            );
-        }
-        console.log(category);
-        setKey((prevKey) => prevKey + 1);
-        updateSuccessMessage("");
-    }, [category]);
-    return { validationSchema, key };
+  useEffect(() => {
+    if (category === "strength") {
+      setValidationSchema(
+        Yup.object().shape({
+          exerciseName: Yup.string().required("Exercise Name is required"),
+          startTime: Yup.string().required("Start Time is required"),
+          endTime: Yup.string().required("End Time is required"),
+          sets: Yup.number().required().min(1, "Sets must be greater than 0"),
+          repsPerSet: Yup.number()
+            .required()
+            .min(1, "Reps must be greater than 0"),
+        })
+      );
+    } else if (category === "cardio") {
+      setValidationSchema(
+        Yup.object().shape({
+          exerciseName: Yup.string().required("Exercise Name is required"),
+          startTime: Yup.string().required("Start Time is required"),
+          endTime: Yup.string().required("End Time is required"),
+        })
+      );
+    }
+    console.log(category);
+    setKey((prevKey) => prevKey + 1);
+    updateSuccessMessage("");
+  }, [category]);
+
+  return { validationSchema, key };
 }
