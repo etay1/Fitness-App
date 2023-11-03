bd1c63e4f8de758798907d03bf2d1d603311b073
Faizan_Rafieuddin <frafi1@brockport.edu>
2023-11-02 22:54:12 -0400

    [ADD] Added a sidebar component

--- fitness_app/src/components/Dashboard/Dashboard.jsx
+++ fitness_app/src/components/Dashboard/Dashboard.jsx
@@ -5,6 +5,7 @@ import AddSubSession from "../AddSubSession/AddSubSession";
 import { useModalState } from "../../hooks/useModalState";
 import "./Dashboard.css";
 import { useEffect } from "react";
+import Sidebar from "../SideBar/SideBar";
 
 const Dashboard = ({ supabase, session }) => {
   const {
@@ -25,136 +26,42 @@ const Dashboard = ({ supabase, session }) => {
 
   return (
     
+
     <div className="dashboard">
-       {/* <div class="sidebar">
-          <div class="menu-btn">
-            <i class="ph-bold ph-caret-left"></i>
-          </div>
-          <div class="head">
-            <div class="user-img">
-              <img src="user.jpg" alt="" />
-            </div>
-            <div class="user-details">
-              <p class="title">web developer</p>
-              <p class="name">John Doe</p>
-            </div>
-          </div>
-          <div class="nav">
-            <div class="menu">
-              <p class="title">Main</p>
-              <ul>
-                <li>
-                  <a href="#">
-                    <i class="icon ph-bold ph-house-simple"></i>
-                    <span class="text">Dashboard</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <i class="icon ph-bold ph-user"></i>
-                    <span class="text">Features</span>
-                    <i class="arrow ph-bold ph-caret-down"></i>
-                  </a>
-                  <ul class="sub-menu">
-                    <li>
-                      <a href="#">
-                        <span class="text">Post</span>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <span class="text">Calendar</span>
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-                <li class="active">
-                  <a href="#">
-                    <i class="icon ph-bold ph-file-text"></i>
-                    <span class="text">Charts</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="#" onClick={openExerciseModal}>
-                    <i class="icon ph-bold ph-calendar-blank"></i>
-                    <span class="text">Add Exercise</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="#" onClick={openAddSubSessionModal}>
-                    <i class="icon ph-bold ph-chart-bar"></i>
-                    <span class="text">Add Subsession</span>
-                    <i class="arrow ph-bold ph-caret-down"></i>
-                  </a>
-                  <ul class="sub-menu">
-                    <li>
-                      <a href="#" onClick={openUserWeightModal}>
-                        <span class="text">Add User Weight</span>
-                      </a>
-                    </li>     
-                  </ul>
-                </li>
-              </ul>
-            </div>
-            <div class="menu">
-              <p class="title">Settings</p>
-              <ul>
-                <li>
-                  <a href="#">
-                    <i class="icon ph-bold ph-gear"></i>
-                    <span class="text">Settings</span>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div class="menu">
-            <p class="title">Account</p>
-            <ul>
-              <li>
-                <a href="#">
-                  <i class="icon ph-bold ph-info"></i>
-                  <span class="text">Help</span>
-                </a>
-              </li>
-              <li>
-              <a href="#" onClick={() => supabase.auth.signOut()}>
-                <i className="icon ph-bold ph-sign-out"></i>
-                <span className="text">Sign Out</span>
-              </a>
-              </li>
-            </ul>
-          </div>
+
+        <div className="sidebar">
+          <Sidebar supabase={supabase} />
         </div>
-        <div class="credits">
-          <h1>
-          {session.user.email}
-          </h1>
-        </div> */}
-      <h1>Welcome {session.user.email} </h1>
-      <button className="form-btn" onClick={() => supabase.auth.signOut()}>Sign Out</button>
-      <button className="form-btn" onClick={openExerciseModal}>Add Exercise</button>
-      <button className="form-btn" onClick={openAddSubSessionModal}>Add Sub Session</button>
-      <button className="form-btn" onClick={openUserWeightModal}>Add User Weight</button>
 
-      <AddExercise
-        isAddExercisePopupOpen={isExerciseModalOpen}
-        closeAddExercisePopup={closeExerciseModal}
-        session={session}
-      />
+        <div className="dashboard-content">
+          <h1>Welcome {session.user.email} </h1>
+          <button className="form-btn" onClick={() => supabase.auth.signOut()}>Sign Out</button>
+          <button className="form-btn" onClick={openExerciseModal}>Add Exercise</button>
+          <button className="form-btn" onClick={openAddSubSessionModal}>Add Sub Session</button>
+          <button className="form-btn" onClick={openUserWeightModal}>Add User Weight</button>
+          <AddExercise
+            isAddExercisePopupOpen={isExerciseModalOpen}
+            closeAddExercisePopup={closeExerciseModal}
+            session={session}
+          />
 
-      <AddSubSession
-        isAddSubSessionPopupOpen={isAddSubSessionModalOpen}
-        closeAddSubSessionPopup={closeAddSubSessionModal}
-        session={session}
-      />
+          <AddSubSession
+            isAddSubSessionPopupOpen={isAddSubSessionModalOpen}
+            closeAddSubSessionPopup={closeAddSubSessionModal}
+            session={session}
+          />
 
-      <AddUserWeight
-        isAddUserWeightPopupOpen={isUserWeightModalOpen}
-        closeAddUserWeightPopup={closeWeightModal}
-        session={session}
-      />
+          <AddUserWeight
+            isAddUserWeightPopupOpen={isUserWeightModalOpen}
+            closeAddUserWeightPopup={closeWeightModal}
+            session={session}
+          />
+        </div>
     </div>
+
+
+    
+    
   );
 };
 
---
+++ fitness_app/src/components/SideBar/SideBar.jsx
@@ -0,0 +1,185 @@
+import { useState } from "react";
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { Link } from "react-router-dom";
+import "react-pro-sidebar/dist/css/styles.css";
+import { tokens } from "./theme";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import SportsGymnasticsIcon from '@mui/icons-material/SportsGymnastics';
+import AddIcon from '@mui/icons-material/Add';
+import LogoutIcon from '@mui/icons-material/Logout';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import TimelineIcon from '@mui/icons-material/Timeline';
+
+const Item = ({ title, to, icon, selected, setSelected }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <MenuItem
+      active={selected === title}
+      style={{
+        color: colors.grey[100],
+      }}
+      onClick={() => setSelected(title)}
+      icon={icon}
+    >
+      <Typography>{title}</Typography>
+      <Link to={to} />
+    </MenuItem>
+  );
+};
+
+const Sidebar = ({supabase}) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [selected, setSelected] = useState("Dashboard");
+
+  return (
+    <Box
+      sx={{
+        "& .pro-sidebar-inner": {
+          background: `${colors.primary[400]} !important`,
+          height: "100vh",
+        },
+        "& .pro-icon-wrapper": {
+          backgroundColor: "transparent !important",
+        },
+        "& .pro-inner-item": {
+          padding: "10px 20px 5px 20px !important",
+        },
+        "& .pro-inner-item:hover": {
+          color: "#868dfb !important",
+        },
+        "& .pro-inner-item:click": {
+          color: "#ffffff !important",
+        },
+        "& .pro-menu-item.active": {
+          color: "#ffffff !important",
+        },
+        
+      }}
+    >
+      <ProSidebar collapsed={isCollapsed}>
+        <Menu iconShape="circle">
+
+          {/* Menu Icon */}
+          <MenuItem
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+            style={{
+              margin: "10px 0 20px 0",
+              color: colors.grey[100],
+            }}
+          >
+            {!isCollapsed && (
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                ml="15px"
+              >
+                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                  <MenuOutlinedIcon />
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
+          <Box paddingLeft={isCollapsed ? undefined : "10%"}>
+            <Item
+              title="Dashboard"
+              to="/"
+              icon={<HomeOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Typography
+              variant="h6"
+              color={colors.grey[300]}
+              sx={{ m: "15px 0 5px 10px" }}
+            >
+              Data
+            </Typography>
+            <Item
+              title="Users"
+              icon={<PeopleOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Sessions"
+              icon={<SportsGymnasticsIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Workouts"
+              icon={<FitnessCenterIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Typography
+              variant="h6"
+              color={colors.grey[300]}
+              sx={{ m: "15px 0 5px 10px" }}
+            >
+              Pages
+            </Typography>
+            <Item
+              title="Add Session"
+              icon={<AddIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="View Exercises"
+              icon={<VisibilityIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Fitness Blog"
+              icon={<HelpOutlineOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Typography
+              variant="h6"
+              color={colors.grey[300]}
+              sx={{ m: "15px 0 5px 10px" }}
+            >
+              Charts
+            </Typography>
+            <Item
+              title="Weight History"
+              icon={<TimelineIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <Item
+              title="Sign Out"
+              icon={<LogoutIcon />}
+              selected={selected}
+              onClick={() => supabase.auth.signOut()}
+              setSelected={setSelected}
+            />
+          </Box>
+        </Menu>
+      </ProSidebar>
+    </Box>
+  );
+};
+
+export default Sidebar;
