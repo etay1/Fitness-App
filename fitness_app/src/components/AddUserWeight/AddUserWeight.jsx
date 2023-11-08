57034c140bd8ffb5b0147cbdfb9e7ac17ffb76f1
etay1 <etay1@brockport.edu>
2023-11-08 12:24:06 -0500

    [FIX] AddUserWeight: title typo

--- fitness_app/src/components/AddUserWeight/AddUserWeight.jsx
+++ fitness_app/src/components/AddUserWeight/AddUserWeight.jsx
@@ -23,7 +23,7 @@ function AddUserWeight({
       <div className="overlay"></div>
       <div className="container">
         <div className="form-ctn">
-          <h1 className="title-form">Create a New Exercise</h1>
+          <h1 className="title-form">Record Weight</h1>
           <UserWeightForm
             closeAddUserWeightPopup={closeAddUserWeightPopup}
             session={session}
