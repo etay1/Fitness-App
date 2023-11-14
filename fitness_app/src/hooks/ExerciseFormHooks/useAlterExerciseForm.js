e57720949b17e31931ef451658a3789a63289aa6
etay1 <etay1@brockport.edu>
2023-11-13 22:16:14 -0500

    [FIX] bugs in Exercise Registry

--- fitness_app/src/hooks/ExerciseFormHooks/useAlterExerciseForm.js
+++ fitness_app/src/hooks/ExerciseFormHooks/useAlterExerciseForm.js
@@ -3,16 +3,24 @@ import { HandleDatabaseError } from "../../utils/HandleDatabaseError";
 import { useSuccessMessage } from "../useSuccessMessage";
 
 export function useAlterExerciseForm(supabase, category) {
-  
   const [isSuccess, setIsSuccess] = useState(false);
   const { successMessage, setSuccessMessage } = useSuccessMessage();
-  
-  const handleAlter = async (values) => {
-    const { exerciseId, exerciseName, caloriesPerRep, caloriesPerDuration, description } = values;
 
-    const tableName = category === "cardio" ? "cardio_exercise" : "weight_exercise";
-    const calorieColumn = category === "cardio" ? "calories_per_unit_duration" : "calories_per_rep";
-    const calories = category === "cardio" ? caloriesPerDuration : caloriesPerRep;
+  const handleAlter = async (values) => {
+    const {
+      exerciseId,
+      exerciseName,
+      caloriesPerRep,
+      caloriesPerDuration,
+      description,
+    } = values;
+
+    const tableName =
+      category === "cardio" ? "cardio_exercise" : "weight_exercise";
+    const calorieColumn =
+      category === "cardio" ? "calories_per_unit_duration" : "calories_per_rep";
+    const calories =
+      category === "cardio" ? caloriesPerDuration : caloriesPerRep;
 
     try {
       const { data, error } = await supabase
