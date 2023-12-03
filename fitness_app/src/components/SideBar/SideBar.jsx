b023fb37a275408eeaa21d2bdee856cefd921487
Ephrem <ekass2@brockport.edu>
2023-12-02 20:49:33 -0500

    fix

--- fitness_app/src/components/SideBar/SideBar.jsx
+++ fitness_app/src/components/SideBar/SideBar.jsx
@@ -169,7 +169,20 @@ const Sidebar = () => {
 								selected={selected}
 								setSelected={setSelected}
 							/>
-
+    						<Typography
+								className='sidebar-typography-account-settings'
+								variant='h6'
+								color={style.getPropertyValue("--light-grey-color")}
+							>
+								Account Settings
+							</Typography>
+							<Item
+								title='Account Settings'
+								icon={<ManageAccountsIcon />}
+								selected={selected}
+								setSelected={setSelected}
+								to={"/Account-Settings"}
+							/>
 							<SignOutItem
 								className='sign-out-button'
 								title='Sign Out'
